d8f20c06123f80a81144f8e730db04ba9026e45b
Moes <m_eser85@hotmail.com>
2021-03-27 14:39:12 +0100

    Exam q8

--- Soru8.py
+++ Soru8.py
@@ -15,3 +15,32 @@
 #        /  \       / \
 #       /    \     /   \
 #      8     12   16   25
+
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+ 
+def derinlik(root):
+    if root is None:
+        return 0
+    else :
+        left = derinlik(root.left)
+        right = derinlik(root.right)
+ 
+        if (left > right):
+            return left+1
+        else:
+            return right+1
+ 
+root = Node(15)
+root.left = Node(10)
+root.right = Node(20)
+root.left.left = Node(8)
+root.left.right = Node(12)
+root.right.left = Node(16)
+root.right.right = Node(15)
+print (f"Maximum Derinlik: {derinlik(root)}")
