b4e8b0ab6c00490acb92d3a17ad7d1d8bdaf457c
MahirCanata <77250816+TahirCanata@users.noreply.github.com>
2021-03-27 14:39:05 +0100

    Add files via upload

---
+++ 4-Soru.py
@@ -0,0 +1,19 @@
+from collections import OrderedDict
+
+def removeDupWithoutOrder(str):
+
+    str = ''.join(str.split())
+    str= "".join(set(str))
+    #str= ''.join(sorted(str))
+
+    return ''.join(sorted(str))
+
+
+def listreverse(str):
+    return str.split(' ')[::-1]
+
+
+if __name__ == "__main__":
+    str = 'monty pythons flying circus'
+    print("Without Order = ", removeDupWithoutOrder(str))
+    print("Reverse List  = ", listreverse(str))
