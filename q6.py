a8a137c34c3d5629478fc2788a4a6c327de5117e
semih <77213397+semihPy@users.noreply.github.com>
2021-03-27 14:45:00 +0100

    Add files via upload
    @secedit

---
+++ q6.py
@@ -0,0 +1,28 @@
+
+# hesap.py dosyasindaki islemler ve bakiye degerlerini import edin
+# islemler listesini okuyup sirasiyla islem tipi ve miktarina gore hesaptaki tutari guncelleyen bir kod yazin.
+# her bir adimi, olusturdugunuz 'islemler.txt' dosyasina yeni birer satir olarak ekleyin. orn. islem tipi para cekme, tutar=100 => txt dosyasinda
+# gorunmesi gereken satir + 100
+# eger islem tipi para cekme ise ve cekilmek istenen miktar halihazirda hesapta olan bakiyeden fazla ise, raise kullanarak exception olusturun ve "hesapta yeterli miktar yoktur"
+# benzeri bir hata mesaji printleyin.
+# en son adimda hesaptaki guncel tutari txt dosyasina yazin (bakiye: miktar) seklinde.
+# tum bu akisi dosya acma/kapama islemleri ve try-except yontemi ile yapin.
+
+# tum kod neticesinde olusturmaniz gereken islemler.txt dosyasinin icerigi asagidaki gibi olmalidir:
+# + 100
+# + 200
+# - 50
+# + 100
+# - 250
+# + 30
+# bakiye: 130
+
+# tum kod neticesinde terminalde gorunmesi gereken degerler:
+# hesapta yeterli bakiye yoktur
+# bakiye: 130
+import hesap
+for i in hesap.islemler:    
+    
+    print(i.values())
+   
+ 
